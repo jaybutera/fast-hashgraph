2e79feac39ff631791244104c52a5e2fd0364987
jaybutera <buterajay@gmail.com>
2019-09-07 17:33:32 -0500

    Using unsafe

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-//type EventId = usize;
-struct EventId {
-    genesis: bool,
-    index: usize,
-}
+type EventId = usize;
 type PeerId = usize;
 type Transaction = u32;
 
 struct Generation<T> {
-    data: T,
+    value: T,
     generation: usize,
 }
 
@@ -18,8 +14,9 @@ struct IndexAllocator {
 }
 impl IndexAllocator {
     fn allocate(&mut self) -> usize {
+        let i = self.latest_idx;
         self.latest_idx += 1;
-        self.latest_idx
+        i
     }
 }
 
@@ -38,15 +35,16 @@ struct Graph {
 
     // Internal tracking of update generations
     generation: usize,
+    allocator: IndexAllocator,
 }
 
-#[derive(Serialize, Deserialize, Clone)] // Clone is temporary for graph unit tests
+//#[derive(Serialize, Deserialize, Clone)] // Clone is temporary for graph unit tests
 enum Event {
     Update {
         creator: PeerId,
         self_parent: PeerId,
         other_parent: Option<PeerId>,
-        txs: Vec<Transaction>,
+        txs: Option< Vec<Transaction> >,
     },
     Genesis{creator: PeerId},
 }
@@ -66,47 +64,110 @@ impl Event {
 impl Graph {
     fn new() -> Self {
         let max = 1000;
+        let allocator = IndexAllocator{ latest_idx: 0 };
+        let mut genesis = Vec::with_capacity(max);
+        let mut self_parent = Vec::with_capacity(max);
+        let mut other_parent = Vec::with_capacity(max);
+        let mut txs = Vec::with_capacity(max);
+        let mut creator = Vec::with_capacity(max);
+        let mut round = Vec::with_capacity(max);
+        let mut famous = Vec::with_capacity(max);
+        let mut witness = Vec::with_capacity(max);
+
+        unsafe {
+            genesis.set_len(max);
+            self_parent.set_len(max);
+            other_parent.set_len(max);
+            txs.set_len(max);
+            creator.set_len(max);
+            round.set_len(max);
+            famous.set_len(max);
+            witness.set_len(max);
+        }
+
         Graph {
-            genesis:      Vec::with_capacity(max),
-            self_parent:  Vec::with_capacity(max),
-            other_parent: Vec::with_capacity(max),
-            txs:          Vec::with_capacity(max),
-            creator:      Vec::with_capacity(max),
-            round:        Vec::with_capacity(max),
-            famous:       Vec::with_capacity(max),
-            witness:      Vec::with_capacity(max),
+            genesis:      genesis,//Vec::with_capacity(max),
+            self_parent:  self_parent,//Vec::with_capacity(max),
+            other_parent: other_parent,//Vec::with_capacity(max),
+            txs:          txs,//Vec::with_capacity(max),
+            creator:      creator,//Vec::with_capacity(max),
+            round:        round,//Vec::with_capacity(max),
+            famous:       famous,//Vec::with_capacity(max),
+            witness:      witness,//Vec::with_capacity(max),
             generation:   0,
+            allocator:    allocator,
         }
     }
 
+    /*
     fn get(&self, eid: &EventId) -> &Event {
     }
+    */
 
     fn add_event(&mut self, e: Event) -> EventId {
+        let eid = self.allocator.allocate();
+
         match e {
-            Genesis => 
+            Event::Genesis{ creator } => {
+                println!("Len: {}", self.genesis.len());
+                self.genesis[eid] = true;
+                self.creator[eid] = creator;
+            },
+            Event::Update{ creator, self_parent, other_parent, txs } => {
+                self.creator[eid] = creator;
+                self.self_parent[eid] = self_parent;
+                self.other_parent[eid] = other_parent;
+                self.txs[eid] = txs;
+            }
         }
+
+        // Update the current generation
+        self.generation += 1;
+
+        eid
     }
 
-    fn is_famous(&self, eid: &EventId) -> bool {
+    fn is_famous(&self, eid: EventId) -> bool {
         //let event = self.get(eid);
 
         // Once an event is famous it won't become unfamous
-        let self.famous[eid.index];
-        if self.famous[eid.index].data == true {
+        let fame = &self.famous[eid];
+        if fame.value == true {
             return true;
         }
 
         // If it's not famous, check if its generation is current enough to trust the data
         let cur = self.generation;
-        if self.get(eid) != cur {
+        if fame.generation == cur {
+            // Should always be false
+            return fame.value;
+        } else {
+            // Need to recheck and update fame status
+
+            return true;
         }
+    }
+
+    /*
+    fn determine_famous(&self, eid: EventId) -> bool {
+        // TODO: Filter all events to only those with round >= eid's round
+
+        // Compute transtive closure with Floyd-Warshall
+        // An event is famous if 2/3 future witnesses strongly see it.
     }
 
     fn is_witness(&self, eid: EventId) -> bool {
     }
+
+    fn strongly_see(&self, x_id: EventId, y_id: EventId) -> bool {
+        // Kruskal's algorithm
+    }
+    */
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut g = Graph::new();
+
+    let e = Event::Genesis { creator: 0 };
+    g.add_event(e);
 }
